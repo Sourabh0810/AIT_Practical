bc466bfca80b645eda193d7a8d9b9c7b751c4aa8
Sourabh0810 <89131314+Sourabh0810@users.noreply.github.com>
2023-07-30 22:33:56 +0530

    Add files via upload

---
+++ Component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-directive-demo',
+  templateUrl: './directive-demo.component.html',
+  styleUrls: ['./directive-demo.component.css']
+})
+export class DirectiveDemoComponent {
+showContent = true;
+}
+Highlight.ts
+
+App-component.ts
+<app-directive-demo></app-directive-demo>
---
+++ ait7.js
@@ -0,0 +1,7 @@
+angular.module('DateTimeApp', [])
+  .controller('DateTimeController', ['$scope', '$interval', function($scope, $interval) {
+    $interval(function() {
+      $scope.currentDate = new Date();
+      $scope.currentTime = new Date();
+    }, 1000);
+  }]);
